f3de1ea7232d11828a3572891fb0af9b330796b3
Kavinda <rdks.kavinda@gmail.com>
2023-10-21 06:30:28 -0700

    server create

--- server/server.js
+++ server/server.js
@@ -1,6 +1,28 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const URL =
+  "mongodb+srv://KavindaRajapaksha:X2z7pku9wIYihAzR@cluster0.xihfyjd.mongodb.net/Portfolio_DB";
+const ConnectMongoDB = (URL) => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(URL);
+    console.log("Successfully Connected to MongoDB");
+  } catch (err) {
+    console.log("MongoDB Connection Failed");
+  }
+};
+ConnectMongoDB(URL);
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS UP AND RUNNING ON PORT ${PORT}`);
+});
