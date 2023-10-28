a603d0e6bb8b06bf1ce57dcdd5d04199af7fc85a
Kavinda <rdks.kavinda@gmail.com>
2023-10-28 23:32:00 +0530

    Finish UI Pages

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import "./Contacts.css";
 
-export default function Contacts() {
+const Contacts = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <div className="contacts-page">
+      <section className="header">
+        <NavBar />
+      </section>
+      <section className="contacts">
+        <form>
+          <h2 className="heading">
+            Contact <span>Me!</span>
+          </h2>
+          <div className="form-group">
+            <div className="row1">
+              <input
+                type="text"
+                placeholder="fullName"
+                className="form-control"
+              />
+              <input type="text" placeholder="email" className="form-control" />
+            </div>
+            <div className="row2">
+              <input type="text" placeholder="age" className="form-control" />
+              <input
+                type="text"
+                placeholder="subject"
+                className="form-control"
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="message"
+              className="form-control"
+              id="message"
+            />
+          </div>
+          <button type="submit" className="btn-message">
+            Send Message
+          </button>
+        </form>
+      </section>
+      <section className="admin-only" id="ak">
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+
+       <button className="adminOnly">Admin only</button>
+      </section>
+      <section className="footer">
+        <Footer />
+      </section>
+    </div>
+  );
+};
+export default Contacts;
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,7 +1,110 @@
-import React from 'react'
-
-export default function Portfolio() {
+import React from "react";
+import NavBar from "../components/NavBar";
+import "./Portfolio.css";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Footer from "../components/Footer";
+const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <div>
+      <section className="header">
+        <NavBar />
+      </section>
+      <h2 className="portfolio-heading">
+            Latest  <span>Projects</span>
+          </h2>
+      <section className="portfolio">
+
+        <div className="portfolio-box">
+        <div className="set-1">
+          <div className="pbox">
+            <img src={require("../assets/portfolio1.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="l"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+
+          <div className="pbox">
+            <img src={require("../assets/portfolio2.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="l"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+
+          <div className="pbox">
+            <img src={require("../assets/portfolio3.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="l"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+</div> <div className="set-2">
+          <div className="pbox">
+            <img src={require("../assets/portfolio4.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="l"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+
+          <div className="pbox">
+            <img src={require("../assets/portfolio5.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="xl"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+
+          <div className="pbox">
+            <img src={require("../assets/portfolio6.jpg")} alt="portfolio1" />
+            <div className="portfolio-layer">
+              <h4>web Designing</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim </p>
+                 <a href="#"> <FontAwesomeIcon
+                icon={faArrowUpRightFromSquare}
+                size="xl"
+                style={{ color: "#fff" }}
+              /></a>
+            </div>
+          </div>
+
+          
+</div>
+
+        </div>
+      </section>
+      <section className="footer">
+        <Footer />
+      </section>
+    </div>
+  );
+};
+
+export default Portfolio;
