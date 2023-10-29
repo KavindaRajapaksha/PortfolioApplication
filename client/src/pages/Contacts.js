38a36d21c0a642b40937cf7da517f8ed1837aed7
Kavinda <rdks.kavinda@gmail.com>
2023-10-29 11:35:47 +0530

    client messages form done

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -2,15 +2,44 @@ import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import "./Contacts.css";
-
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Contacts = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      fullName,
+      email,
+      age,
+      subject,
+      message,
+    };
+    try {
+      await axios.post("http://localhost:8000/clients/add", data);
+      alert("Message sent");
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="contacts-page">
       <section className="header">
         <NavBar />
       </section>
       <section className="contacts">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2 className="heading">
             Contact <span>Me!</span>
           </h2>
@@ -18,24 +47,52 @@ const Contacts = () => {
             <div className="row1">
               <input
                 type="text"
+                name="fullName"
                 placeholder="fullName"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
                 className="form-control"
+                required
               />
-              <input type="text" placeholder="email" className="form-control" />
-            </div>
-            <div className="row2">
-              <input type="text" placeholder="age" className="form-control" />
               <input
                 type="text"
-                placeholder="subject"
+                name="email"
+                placeholder="email"
+                value={email}
                 className="form-control"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="row2">
+              <input
+                type="number"
+                name="age"
+                placeholder="age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                className="form-control"
+                required
+              />
+              <input
+                type="text"
+                name="subject"
+                placeholder="subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                className="form-control"
+                required
               />
             </div>
             <input
               type="text"
               placeholder="message"
+              name="message"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
               className="form-control"
               id="message"
+              required
             />
           </div>
           <button type="submit" className="btn-message">
@@ -44,9 +101,21 @@ const Contacts = () => {
         </form>
       </section>
       <section className="admin-only" id="ak">
-        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+          If you are going to use a passage of Lorem Ipsum, you need to be sure
+          there isn't anything embarrassing hidden in the middle of text. All
+          the Lorem Ipsum generators on the Internet tend to repeat predefined
+          chunks as necessary, making this the first true generator on the
+          Internet. It uses a dictionary of over 200 Latin words, combined with
+          a handful of model sentence structures, to generate Lorem Ipsum which
+          looks reasonable. The generated Lorem Ipsum is therefore always free
+          from repetition, injected humour, or non-characteristic words etc.
+        </p>
 
-       <button className="adminOnly">Admin only</button>
+        <button className="adminOnly">Admin only</button>
       </section>
       <section className="footer">
         <Footer />
